005174d62485d4657ebeb2e8ee41dd9a9b078bf5
Nivanth Naricitty <nivanthn@gmail.com>
2026-01-19 14:34:20 +0800

    Added Greeting

--- src/main/java/Biscuit.java
+++ src/main/java/Biscuit.java
@@ -1,5 +1,11 @@
 public class Biscuit {
     public static void main(String[] args) {
-        
+        System.out.println("___________________________________");
+        System.out.println("Hello! I'm Biscuit");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("___________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("___________________________________");
     }
 }

d4c51b4becc0847a85d297fdc6868f1dec9dcb7c
Nivanth Naricitty <nivanthn@gmail.com>
2026-01-19 15:42:32 +0800

    Add marking feature

--- src/main/java/Biscuit.java
+++ src/main/java/Biscuit.java
@@ -3,27 +3,45 @@ import java.util.ArrayList;
 public class Biscuit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> arr = new ArrayList<>();
+        ArrayList<Task> arr = new ArrayList<>();
 
         System.out.println("___________________________________");
         System.out.println("Hello! I'm Biscuit");
         System.out.println("What can I do for you?");
         System.out.println("___________________________________");
-        String hi = sc.nextLine();
-        while(!hi.toLowerCase().equals("bye")) {
+        String input = sc.nextLine();
+        while(!input.toLowerCase().equals("bye")) {
             System.out.println("    ___________________________________");
-            if(hi.toLowerCase().equals("list")) {
+            if(input.toLowerCase().equals("list")) {
                 for (int i = 0; i < arr.size(); i++) {
-                    System.out.println("     " + i + ". " + arr.get(i));
+                    System.out.println("     " + (i+1) + ". " + arr.get(i));
                 }
+            } else if(input.toLowerCase().equals("mark")) {
+                System.out.println("Which task would you like to mark done?");
+                int index = Integer.parseInt(sc.nextLine());
+                Task task = arr.get(index - 1);
+                task.mark();
+                System.out.println("Ok! The task below is marked done");
+                System.out.println(task);
+                
+            } else if(input.toLowerCase().equals("unmark")) {
+                System.out.println("Which task would you like to unmark?");
+                int index = Integer.parseInt(sc.nextLine());
+                Task task = arr.get(index - 1);
+                task.unmark();
+                System.out.println("Ok! The task below is marked undone");
+                System.out.println(task);
+                
             } else {
-                arr.add(hi);
-                System.out.println("    added: " + hi);
-            }
+                Task task = new Task(input);
+                arr.add(task);
+                System.out.println("    added: " + task);
+            } 
+
             System.out.println("    ___________________________________");
-            hi = sc.nextLine();
+            input = sc.nextLine();
         }
-        System.out.println("        Bye. Hope to see you again soon!");
+        System.out.println("    Bye. Hope to see you again soon!");
         System.out.println("    ___________________________________");
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        isDone = true;
+    }
+
+    public void unmark() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone ? "[X] " : "[ ] ") + description;
+    }
+
+    
+}

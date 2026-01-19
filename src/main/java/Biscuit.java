9fdef1a2335d55eb07076b7b14626cb289d7ce04
Nivanth Naricitty <nivanthn@gmail.com>
2026-01-19 14:51:30 +0800

    Added echo feature

--- src/main/java/Biscuit.java
+++ src/main/java/Biscuit.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
 public class Biscuit {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("___________________________________");
         System.out.println("Hello! I'm Biscuit");
         System.out.println("What can I do for you?");
-        System.out.println();
         System.out.println("___________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("___________________________________");
+        String hi = sc.nextLine();
+        while(!hi.toLowerCase().equals("bye")) {
+            System.out.println("    ___________________________________");
+            System.out.println("    " + hi);
+            System.out.println("    ___________________________________");
+            hi = sc.nextLine();
+        }
+        System.out.println("        Bye. Hope to see you again soon!");
+        System.out.println("    ___________________________________");
     }
 }

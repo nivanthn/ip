568ffbc28388eceacf54602736c713ff5e044b54
Nivanth Naricitty <nivanthn@gmail.com>
2026-01-30 12:45:02 +0800

    Add Parser and TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+
+    private final List<Task> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+
+    public TaskList(List<Task> loadedTasks) {
+        tasks = new ArrayList<>(loadedTasks);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    public Task remove(int index) {
+        return tasks.remove(index);
+    }
+
+    public List<Task> asList() {
+        return List.copyOf(tasks);
+    }
+}
